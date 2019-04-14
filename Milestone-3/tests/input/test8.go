35f4d16782f4bc78e054c9a01f47fa8f7e927fab
pardhu225 <chowdary1969@gmail.com>
2019-04-14 18:29:21 +0530

    arraysize generated

--- Milestone-3/tests/input/test8.go
+++ Milestone-3/tests/input/test8.go
@@ -3,19 +3,22 @@ import "fmt";
 
 func main() {
 
-	var i int_t = 5;
-	var j int_t;
-	var t int_t = 1;
-	var a int_t = 1;
-	for j=a;j<i;j=j+a
-	{
-		t = t*j;
-	};
-	for j=a;j<i;j=j+a
-	{
-		t = t*j;
-	};
-	print(t);
+	var i[10] int_t;
+	var j[100] int_t;
+	var k[120] int_t;
+	var t int_t;
+	// var j int_t;
+	// var t int_t = 1;
+	// var a int_t = 1;
+	// for j=a;j<i;j=j+a
+	// {
+	// 	t = t*j;
+	// };
+	// for j=a;j<i;j=j+a
+	// {
+	// 	t = t*j;
+	// };
+	// print(t);
 	
 	return;
 };
